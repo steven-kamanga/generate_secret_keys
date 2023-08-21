328f0ccfc385b600b8931f399f111ae02e4f333a
steven-kamanga <kamangasteven00@gmail.com>
2023-08-21 21:49:20 +0200

    added AES

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/aes.h>
+#include <openssl/rand.h>
 #include "generator.h"
 #include "file_writer.h"
+
+void print_hex(const unsigned char *data, size_t length) {
+    for (size_t i = 0; i < length; ++i) {
+        printf("%02x", data[i]);
+    }
+    printf("\n");
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 9) {
@@ -26,17 +35,42 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Generate AES key
+    unsigned char aes_key[32]; // 256-bit key
+    if (!RAND_bytes(aes_key, sizeof(aes_key))) {
+        fprintf(stderr, "Error generating random key\n");
+        return 1;
+    }
+
+    // Use AES key to generate final key
     const char *salt = "12345678!@#$)(_^&*()";
     char *generated_key = generate_key(length, from, to, salt);
 
-    if (write_to_file(output_path, generated_key) != 0) {
+    // Perform AES encryption
+    AES_KEY aes_enc_key;
+    AES_set_encrypt_key(aes_key, 256, &aes_enc_key);
+
+    unsigned char iv[AES_BLOCK_SIZE];
+    RAND_bytes(iv, AES_BLOCK_SIZE);
+
+    unsigned char *encrypted_key = (unsigned char *)malloc(length + AES_BLOCK_SIZE);
+    AES_cbc_encrypt((unsigned char *)generated_key, encrypted_key, length, &aes_enc_key, iv, AES_ENCRYPT);
+
+    // Print encrypted key in hexadecimal format
+    printf("Encrypted Key (Hex): ");
+    print_hex(encrypted_key, length + AES_BLOCK_SIZE);
+
+    // Write encrypted key to file
+    if (write_to_file(output_path, (char *)encrypted_key) != 0) {
         fprintf(stderr, "Error writing to file\n");
         return 1;
     }
 
-    printf("Key generated and written to file: %s\n", output_path);
+    printf("Encrypted key written to file: %s\n", output_path);
 
+    // Clean up
     free(generated_key);
+    free(encrypted_key);
 
     return 0;
 }
